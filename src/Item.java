1ca9ec85068dc85d2125b218fe03e1e274190772
roshanroy11 <roshanroyvarghese30@gmail.com>
2023-01-20 19:56:31 -0500

    " changes"

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,14 @@
 public class Item {
-    public static void main(String[] args) {
-        System.out.println("Try");
+    protected String name;
+    protected String description;
+    protected int value;
+
+    public Item(String name,String description,int value){
+        this.name = name;
+        this.description = description;
+        this.value = value;
+    }
+    public String toString(){
+        return String.format("%s \n=====\n %s \nValue: %d \n",this.name, this.description, this.value);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Item i1 = new Item("item name", "item desc", 10);
+        Item i2 = new Item("item name", "item desc", 11);
+        Weapon w1 = new Weapon("weapon name", "weapon description", 10,20);
+        Gold goldBar = new Gold(40);
+        Sword magicSword = new Sword();
+        Pillow softPillow = new Pillow();
+        Mace bigMace = new Mace();
+
     }
 }
