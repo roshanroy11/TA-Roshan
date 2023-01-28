fffc517bd532eb6c32e7dbe1c92a126290cddf80
roshanroy11 <roshanroyvarghese30@gmail.com>
2023-01-28 15:18:18 -0500

    " change"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Item i1 = new Item("item name", "item desc", 10);
-        Item i2 = new Item("item name", "item desc", 11);
         Weapon w1 = new Weapon("weapon name", "weapon description", 10,20);
         Gold goldBar = new Gold(40);
         Sword magicSword = new Sword();
