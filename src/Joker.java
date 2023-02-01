4689fbf9e00a440f5d267403413ff17b14a5b9e8
roshanroy11 <roshanroyvarghese30@gmail.com>
2023-02-01 14:36:56 -0500

    " change"

---
+++ src/Joker.java
@@ -0,0 +1,5 @@
+public class Joker extends Enemy{
+    public Joker(){
+        super("Joker",40,25);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,17 @@ public class Main {
         Item i1 = new Item("item name", "item desc", 10);
         Weapon w1 = new Weapon("weapon name", "weapon description", 10,20);
         Gold goldBar = new Gold(40);
+        //Weapons:
         Sword magicSword = new Sword();
         Pillow softPillow = new Pillow();
         Mace bigMace = new Mace();
+        //Enemies:
+        Ogre greenOgre = new Ogre();
+        Ogre redOgre = new Ogre();
+        Zombie slowZombie = new Zombie();
+        Zombie fastZombie = new Zombie();
+        Dog spotDog = new Dog();
+        Joker smartClown = new Joker();
 
     }
 }
