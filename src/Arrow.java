936f33556932f706483f01b9c2cce2ec010ffcbd
roshanroy11 <roshanroyvarghese30@gmail.com>
2023-04-28 12:46:59 -0400

    Final Updated

--- src/Arrow.java
+++
@@ -1,5 +0,0 @@
-public class Arrow extends Weapon{
-    public Arrow(){
-        super("Arrow", "A Arrow. A killer stick which has a sharp shaft in front and a feather at the back", 15, 20);
-    }
-}
